8784c3105a2f4dd22965c01e36c1a88998168150
mfkeles <mkeles@ucla.edu>
2023-07-10 13:33:45 -0400

    utilized Dash instead of tkinter

--- ezlabel.py
+++ ezlabel.py
@@ -1,183 +1,251 @@
-import tkinter as tk
-from tkinter import filedialog
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output, State
+import plotly.graph_objects as go
 import pandas as pd
+import os
+import base64
+import io
 import pickle
-import numpy as np
+
+app = dash.Dash(__name__)
+
+N = 30  # Number of points to mark before and after the clicked point
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=False
+    ),
+    dcc.Store(id='intermediate-data'),
+    dcc.Store(id='column-index', data=0),
+    dcc.Store(id='prev-clicks-store', data=0),
+    dcc.Store(id='next-clicks-store', data=0),
+    dcc.Store(id='annotations', data=[]),  # Store for annotations
+    dcc.Graph(id='figure'),
+    html.Div(id='clickdata', children="No click registered yet."),
+    html.Div(id='output-data-upload'),
+    html.Div(id='save-confirm'),
+    dcc.Dropdown(id='column-dropdown'),
+    html.Button('Previous Column', id='prev-button'),
+    html.Button('Next Column', id='next-button'),
+    html.Button('Save Annotations', id='save-button'),
+    html.Button('Load Annotations', id='load-button'),
+    html.Div(id='annotations-list')
+
+])
 
 
-class DraggableLine:
-    def __init__(self, line, other_line, window_shade):
-        self.line = line
-        self.other_line = other_line
-        self.window_shade = window_shade
-        self.press = None
-        self.connections = []
-        self.connections.append(line.figure.canvas.mpl_connect('button_press_event', self.on_press))
-        self.connections.append(line.figure.canvas.mpl_connect('button_release_event', self.on_release))
-        self.connections.append(line.figure.canvas.mpl_connect('motion_notify_event', self.on_motion))
+@app.callback(
+    Output('intermediate-data', 'data'),
+    Output('column-dropdown', 'options'),
+    Output('output-data-upload', 'children'),
+    [Input('upload-data', 'contents'),
+     Input('upload-data', 'filename')])
+def update_data(contents, filename):
+    if contents is None:
+        raise dash.exceptions.PreventUpdate
 
-    def on_press(self, event):
-        if event.inaxes != self.line.axes:
-            return
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            data = {
+                'df': df.to_json(orient='split', index=False),
+                'columns': [col for col in df.columns if col not in ['start_index', 'stop_index', 'region']],
+                'filename': filename.split('.')[0]  # add filename to data
+            }
+    except Exception as e:
+        return dash.no_update
 
-        contains, _ = self.line.contains(event)
-        if not contains:
-            return
+    column_options = [{'label': column, 'value': column} for column in data['columns']]
 
-        self.press = (self.line.get_xdata()[0], self.other_line.get_xdata()[0], event.xdata)
-
-    def on_motion(self, event):
-        if event.inaxes != self.line.axes:
-            return
-
-        dx = event.xdata - self.press[2]
-        new_line_xdata = [self.press[0] + dx]
-        new_other_line_xdata = [self.press[1] + dx]
-
-        self.line.set_xdata(new_line_xdata)
-        self.other_line.set_xdata(new_other_line_xdata)
-
-        self.line.figure.canvas.draw()
-
-    def on_release(self, event):
-        self.press = None
+    return data, column_options, f'Successfully loaded {filename}'  # return only three values here
 
 
-class Application(tk.Tk):
-    def __init__(self):
-        tk.Tk.__init__(self)
-
-        self.data = None  # To store the loaded data
-        self.figure = Figure(figsize=(5, 4), dpi=100)  # The Figure object for the plot
-        self.window_size = 45
-        self.window_start = None
-        self.window_end = None
-        self.window_shade = None
-        self.draggable_start = None
-        self.draggable_end = None
-
-        # Create the dropdowns
-        self.key_var = tk.StringVar(self)
-        self.key_var.trace('w', self.update_rows_range)  # Add callback for when the selected key changes
-        self.column_var = tk.StringVar(self)
-        self.column_var.trace('w', self.update_plot)  # Update plot when selected column changes
-        self.key_dropdown = tk.OptionMenu(self, self.key_var, [])
-        self.column_dropdown = tk.OptionMenu(self, self.column_var, [])
-        self.key_dropdown.pack()
-        self.column_dropdown.pack()
-
-        # Create the Spinbox for the row index
-        self.row_index_var = tk.StringVar(self)  # Create a StringVar for the Spinbox
-        self.row_index_var.trace('w', self.update_plot)  # Update plot when selected row changes
-        self.row_index = tk.Spinbox(self, textvariable=self.row_index_var, from_=0, to=0)
-        self.row_index.pack()
-
-        # Create the canvas for the plot
-        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
-        self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-        # Create the button
-        self.button = tk.Button(self, text="Load data", command=self.load_data)
-        self.button.pack()
-
-        self.window_visible = False
-
-    def show_window(self):
-        if self.window_visible:
-            if self.draggable_start is not None:
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[0])
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[1])
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[2])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[0])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[1])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[2])
-            if self.window_start is not None:
-                self.window_start.remove()
-            if self.window_end is not None:
-                self.window_end.remove()
-            if self.window_shade is not None:
-                self.window_shade.remove()
-
-        self.window_start = self.ax.axvline(x=0, color='r')
-        self.window_end = self.ax.axvline(x=self.window_size, color='r')
-
-        x = np.array([self.window_start.get_xdata()[0], self.window_end.get_xdata()[0]])
-        y = np.array([self.ax.get_ylim()[0], self.ax.get_ylim()[1]])
-        self.window_shade = self.ax.fill_between(x, y, alpha=0.3, color='gray')
-
-        self.draggable_start = DraggableLine(self.window_start, self.window_end, self.window_shade)
-        self.draggable_end = DraggableLine(self.window_end, self.window_start, self.window_shade)
-
-        self.canvas.draw()
-        self.window_visible = True
-
-    def update_rows_range(self, *args):
-        if self.data is not None:  # Check if data has been loaded
-            key = self.key_var.get()
-            self.row_index.config(to=len(self.data[key]) - 1)
-            self.row_index_var.set('0')  # Set the Spinbox value to 0
-
-    def load_data(self):
-        file_path = filedialog.askopenfilename()
-        with open(file_path, 'rb') as file:
-            self.data = pickle.load(file)
-
-        # Update the dropdowns with the keys and columns
-        keys = list(self.data.keys())
-        columns = [col for col in list(self.data[keys[0]].columns) if col not in ['start_index', 'stop_index', 'region']]
-        self.key_dropdown["menu"].delete(0, "end")
-        self.column_dropdown["menu"].delete(0, "end")
-        for key in keys:
-            self.key_dropdown["menu"].add_command(label=key, command=tk._setit(self.key_var, key))
-        for column in columns:
-            self.column_dropdown["menu"].add_command(label=column, command=tk._setit(self.column_var, column))
-
-        # Set initial values for the dropdowns
-        self.key_var.set(keys[0])
-        self.column_var.set(columns[0])
-
-        # Update Spinbox range
-        self.row_index.config(to=len(self.data[keys[0]]) - 1)  # Set the range of the Spinbox
-        self.row_index_var.set('0')  # Set the Spinbox value to 0
-        self.update_idletasks()  # Force Spinbox to update
-
-        self.button_show_window = tk.Button(self, text="Show window", command=self.show_window)
-        self.button_show_window.pack()
-
-    def update_plot(self, *args):
-        key = self.key_var.get()
-        column = self.column_var.get()
-
-        if column:
-            df = self.data[key]
-            try:
-                row_idx = int(self.row_index.get())
-            except ValueError:
-                row_idx = 0
-
-            if self.draggable_start is not None:
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[0])
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[1])
-                self.draggable_start.line.figure.canvas.mpl_disconnect(self.draggable_start.connections[2])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[0])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[1])
-                self.draggable_end.line.figure.canvas.mpl_disconnect(self.draggable_end.connections[2])
-                self.figure.clear()
-
-            self.ax = self.figure.subplots()
-            self.ax.plot(df[column].iloc[row_idx])
-
-            self.ax.set_xticks(np.arange(0, len(df[column].iloc[row_idx]), step=self.window_size))
-            self.ax.set_xlabel("Index")
-
-            if self.window_visible:
-                self.show_window()
-
-            self.canvas.draw()
 
 
-app = Application()
-app.mainloop()
+@app.callback(
+    Output('clickdata', 'children'),
+    [Input('figure', 'clickData')],
+    [State('column-dropdown', 'value'),
+     State('annotations', 'data'),
+     State('intermediate-data', 'data')])
+def display_click_data(clickData, selected_column, annotations, data):
+    if clickData is None:
+        return "No click registered yet."
+    else:
+        clicked_x = clickData['points'][0]['x']
+        clicked_y = clickData['points'][0]['y']
+
+        # Add the clicked point to the annotations
+        new_annotation = {
+            'column': selected_column,
+            'index': clicked_x,
+            'value': clicked_y
+        }
+        annotations.append(new_annotation)
+
+        # Update the stored annotations
+        filename = data["filename"]  # Use the filename of the uploaded CSV file
+        with open(f'{filename}.pkl', 'wb') as f:
+            pickle.dump(annotations, f)
+
+        return f"Clicked point: x={clicked_x}, y={clicked_y}"
+
+
+@app.callback(
+    Output('figure', 'figure'),
+    Output('column-index', 'data'),
+    Output('prev-clicks-store', 'data'),
+    Output('next-clicks-store', 'data'),
+    Output('column-dropdown', 'value'),
+    [Input('prev-button', 'n_clicks'),
+     Input('next-button', 'n_clicks'),
+     Input('column-dropdown', 'value'),
+     Input('intermediate-data', 'data'),
+     Input('annotations', 'data')],
+    [State('column-index', 'data'),
+     State('prev-clicks-store', 'data'),
+     State('next-clicks-store', 'data')]
+)
+def update_figure(prev_clicks, next_clicks, selected_column, data, annotations, column_index, prev_clicks_store,
+                  next_clicks_store):
+    ctx = dash.callback_context
+    if not ctx.triggered or 'intermediate-data.data' in [x['prop_id'] for x in ctx.triggered]:
+        # if the callback was not triggered or if it was triggered by data upload
+        # select the first column to plot
+        if data is None:
+            raise dash.exceptions.PreventUpdate
+
+        df = pd.read_json(data['df'], orient='split')
+        columns = data['columns']
+        selected_column = columns[0]
+        column_index = 0
+        prev_clicks = 0
+        next_clicks = 0
+    else:
+        df = pd.read_json(data['df'], orient='split')
+        columns = data['columns']
+
+        prev_clicks = prev_clicks or 0
+        next_clicks = next_clicks or 0
+
+        if ctx.triggered[0]['prop_id'] == 'column-dropdown.value':
+            column_index = columns.index(selected_column)
+        else:
+            prev_diff = prev_clicks - prev_clicks_store
+            next_diff = next_clicks - next_clicks_store
+            next_column_index = (column_index + next_diff - prev_diff) % len(columns)
+            selected_column = str(columns[next_column_index])
+            column_index = next_column_index
+
+    y_data = df[int(selected_column)]  # Change this line
+    y_data = y_data.dropna()
+
+    x_data = list(range(len(y_data)))
+    # Create the figure
+    figure = go.Figure()
+
+    # Original data without the highlighted section
+    original_trace = go.Scatter(x=x_data, y=y_data, mode='lines+markers',showlegend=False)
+
+    # Add the original trace
+    figure.add_trace(original_trace)
+
+    for annotation in annotations:
+        if annotation['column'] == selected_column:  # change 'col_name' to 'column'
+            idx = annotation['index']
+            lower_bound = max(0, idx - N)
+            upper_bound = min(len(y_data), idx + N)
+
+            # Highlighted section of the data
+            highlighted_trace = go.Scatter(
+                x=list(range(lower_bound, upper_bound)),
+                y=y_data[lower_bound: upper_bound],
+                mode='lines+markers',
+                marker=dict(color='red'),  # Use marker to specify color for points
+                showlegend=False
+            )
+
+            # Add the highlighted trace
+            figure.add_trace(highlighted_trace)
+
+    return figure, column_index, prev_clicks, next_clicks, selected_column
+
+@app.callback(
+    Output('annotations', 'data'),
+    [Input('figure', 'clickData'),
+     Input('load-button', 'n_clicks')],
+    [State('annotations', 'data'),
+     State('column-dropdown', 'value'),
+     State('intermediate-data', 'data')])
+def update_annotations(clickData, n_clicks, annotations, selected_column, data):
+    ctx = dash.callback_context
+
+    # if the callback was triggered by 'load-button', load annotations
+    if ctx.triggered and 'load-button' in ctx.triggered[0]['prop_id']:
+        if n_clicks is None or data is None:
+            raise dash.exceptions.PreventUpdate
+
+        # Load annotations
+        filename = data["filename"]
+        try:
+            with open(f'{filename}.pkl', 'rb') as f:
+                annotations = pickle.load(f)
+        except FileNotFoundError:
+            annotations = []
+
+    # else, the callback was triggered by 'figure.clickData', add the new annotation
+    else:
+        if clickData is None or selected_column is None:
+            raise dash.exceptions.PreventUpdate
+
+        clicked_x = clickData['points'][0]['x']
+        clicked_y = clickData['points'][0]['y']
+
+        new_annotation = {
+            'index': clicked_x,  # change 'x' to 'index'
+            'value': clicked_y,  # change 'y' to 'value'
+            'column': selected_column  # change 'col_name' to 'column'
+        }
+
+        if new_annotation not in annotations:
+            annotations.append(new_annotation)
+
+    return annotations
+
+@app.callback(
+    Output('save-confirm', 'children'),
+    Input('save-button', 'n_clicks'),
+    State('annotations', 'data'),
+    State('intermediate-data', 'data'))   # Use intermediate-data to get filename
+def save_annotations(n_clicks, annotations, data):
+    if n_clicks is None or data is None:
+        raise dash.exceptions.PreventUpdate
+
+    # extract filename from data
+    filename = data['filename']
+    with open(f'{filename}.pkl', 'wb') as f:
+        pickle.dump(annotations, f)
+
+    return f'Successfully saved annotations for {filename}'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
